0f6464e72d4961b60199fa3cf96daea0c6c4fcbd
Stephen Pang <stephenpangqy@gmail.com>
2024-04-23 17:14:40 +0800

    Modified ReceiptDataGrid

--- whopaidlah_frontend/src/components/ReceiptDataGrid.js
+++ whopaidlah_frontend/src/components/ReceiptDataGrid.js
@@ -1,6 +1,7 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback, useMutation } from "react";
 import {
-    Snackbar
+    Snackbar,
+    Alert
 } from "@mui/material";
 import { 
     DataGrid,
@@ -11,14 +12,34 @@ function ReceiptDataGrid(props) {
     const [receiptDataRows, setReceiptDataRows] = useState([]);
     const [columns, setColumns] = useState(["Item", "Quantity", "Cost (Item x Quantity)"]);
     const [idsToDelete, setIdsToDelete] = useState([]);
+    const [primaryKeys, setPrimaryKeys] = useState([]);
 
     const [snackbar, setSnackbar] = useState(null);
+
+    const mutateRow = useMutation();
+
+    const processRowUpdate = useCallback(
+		async (newRow) => {
+			const response = await mutateRow(newRow);
+			return response;
+		},
+		[mutateRow, columns, primaryKeys]
+	);
+
+    const handleProcessRowUpdateError = useCallback((error) => {
+		setSnackbar({ children: error.message, severity: "error" });
+	}, []);
+
+    const handleCloseSnackbar = () => setSnackbar(null);
 
     useEffect(() => {
         // Populate with initial data
         let newReceiptDataRows = [];
         for (let receiptDataObj of props.receiptData) {
             console.log(receiptDataObj);
+            if (newReceiptDataRows.find((innerList) => innerList[0] === receiptDataObj.description)) {
+
+            }
             newReceiptDataRows.push([receiptDataObj.description, receiptDataObj.quantity, receiptDataObj.amount_line])
         }
         // CONTINUE
@@ -31,7 +52,24 @@ function ReceiptDataGrid(props) {
                 rows={receiptDataRows}
                 columns={columns}
                 setIdsToDelete={setIdsToDelete}
+                getRowId={(row) => primaryKeys.map((pk) => row[pk]).join(",")}
+				checkboxSelection={true}
+                processRowUpdate={processRowUpdate}
+                onProcessRowUpdateError={handleProcessRowUpdateError}
+				experimentalFeatures={{ newEditingApi: true }}
+                onSelectionModelChange={(ids) => {
+					setIdsToDelete(ids);
+				}}
             />
+            {!!snackbar && (
+				<Snackbar
+					open
+					anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+					onClose={handleCloseSnackbar}
+				>
+					<Alert {...snackbar} onClose={handleCloseSnackbar} />
+				</Snackbar>
+			)}
         </div>
     );
 }
